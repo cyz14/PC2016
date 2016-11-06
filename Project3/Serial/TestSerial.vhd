d076e9c764629b77e630833a47cea125d6cbe130
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-07 01:23:28 +0800

    完成了串口实验的基本任务

--- Project3/Serial/TestSerial.vhd
+++ Project3/Serial/TestSerial.vhd
@@ -19,7 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -30,83 +31,85 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity TestSerial is
-    Port ( clk : in  STD_LOGIC;
-           rst : in  STD_LOGIC;
-           sw : in  STD_LOGIC_VECTOR (7 downto 0);
-           led : out  STD_LOGIC_VECTOR (7 downto 0);
-           ram1data : inout  STD_LOGIC_VECTOR (7 downto 0);
-           ram1oe : out  STD_LOGIC;
-           ram1we : out  STD_LOGIC;
-           ram1en : out  STD_LOGIC;
-           data_ready : out  STD_LOGIC;
-           rdn : out  STD_LOGIC;
-           tbre : out  STD_LOGIC;
-           tsre : out  STD_LOGIC;
-           wrn : out  STD_LOGIC);
+    Port ( clk :        in     STD_LOGIC;
+           rst :        in     STD_LOGIC;
+           -- sw :         in     STD_LOGIC_VECTOR (15 downto 8); -- not used in the expriment
+           led :        out    STD_LOGIC_VECTOR (7 downto 0);
+           ram1data :   inout  STD_LOGIC_VECTOR (7 downto 0);
+           ram1oe :     out    STD_LOGIC;
+           ram1we :     out    STD_LOGIC;
+           ram1en :     out    STD_LOGIC;
+           data_ready : in     STD_LOGIC;
+           rdn :        out    STD_LOGIC;
+           tbre :       in     STD_LOGIC;
+           tsre :       in     STD_LOGIC;
+           wrn :        out    STD_LOGIC);
 end TestSerial;
 
 architecture Behavioral of TestSerial is
-	type ControlState is (c_st0, c_st1, c_st2);
-	type ReadState is (r_st0, r_st1, r_st2, r_st3, r_st4);
-	type WriteState is (w_st0, w_st1, w_st2, w_st3);
+    type State is (st0, r_st1, r_st2, r_st3, w_st0, w_st1, w_st2, w_st3, w_st4);
 
-    signal st_top : ControlState := c_st0;
-    signal st_read : ReadState := r_st0;
-    signal st_write : WriteState := w_st0;
+    signal cur_st : State := st0;
     
-    signal temp_led : STD_LOGIC_VECTOR(7 downto 0);
+    signal temp_led   : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+    signal temp_data  : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+	 
 begin
-	ram1en <= '1';
+    ram1en <= '1';
     
     top_control: process(clk, rst)
     begin
         if (rst = '0') then
-            st_top <= c_st0;
-            st_write <= w_st0;
-            led <= "0000000";
-            
+            temp_led <= "00000000";
+            ram1en <= '1';
+            ram1we <= '1';
+            ram1oe <= '1';
+            wrn <= '1';
+			cur_st <= st0;
         elsif rising_edge(clk) then
-            case st_top is
-                when c_st0 => 
-                    st_top <= c_st1;
-                when c_st1 =>
-                    st_top <= c_st2;
-                when others =>
-                    st_top <= c_st0;
-            end case;
-        end if;
-    end process;
-    
-    read_control: process(clk, rst)
-    begin
-        if (rst = '0') then
-            rdn <= '1';
-            ram1data <= "ZZZZZZZZ";
-            st_read <= r_st0;
-        elsif rising_edge(clk) then
-            case st_read is
-                when r_st0 =>
-                    st_read <= r_st1;
+            case cur_st is
+                when st0 =>
+                    cur_st <= r_st1;
                 when r_st1 =>
+                    rdn <= '1';
+                    ram1data <= (Others => 'Z');
+                    cur_st <= r_st2;
+                when r_st2 =>
                     if data_ready = '1' then
                         rdn <= '0';
-                        st_read <= r_st2;
+                        cur_st <= r_st3;
+                    else
+                        cur_st <= r_st1;
                     end if;
-                when r_st2 =>
+                when r_st3 =>
                     temp_led <= ram1data;
-                    st_read <= r_st0; 
+                    temp_data <= ram1data;
+                    cur_st <= w_st0;
+                    rdn <= '1';
+                when w_st0 =>
+                    ram1data <= temp_data + 1; -- may overflow here , but doesn't matter in test
+                    cur_st <= w_st1;
+                when w_st1 =>
+                    wrn <= '0';
+                    cur_st <= w_st2;
+                when w_st2 =>
+                    wrn <= '1';
+                    cur_st <= w_st3;
+                when w_st3 =>
+                    if tbre = '1' then
+                        cur_st <= w_st4;
+                    else
+                        cur_st <= w_st3;
+                    end if;
+                when w_st4 =>
+                    if tsre = '1' then
+                        cur_st <= st0;
+                    else
+                        cur_st <= w_st4;
+                    end if;
                 when others =>
-                    st_read <= r_st0;
+                    cur_st <= st0;
             end case;
-        end if;
-    end process;
-
-    write_control: process(clk, rst)
-    begin
-        if (rst = '0') then
-            st_write <= w_st0;
-        elsif rising_edge(clk) then
-
         end if;
     end process;
 
