abb41bd62af18e36e0a6bda884e94c5d9aeaa20c
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-07 18:24:50 +0800

    Change clk to 11.9MHz clock and complete experiment

--- Project3/IOSerial/TestIOSerial.vhd
+++ Project3/IOSerial/TestIOSerial.vhd
@@ -32,13 +32,10 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity TestIOSerial is
-    Port ( clk : in  STD_LOGIC;   -- hand clk
+    Port (
            comclk : in STD_LOGIC; -- 11.9MHz
            rst : in  STD_LOGIC;   -- hand reset
            sw : in  STD_LOGIC_VECTOR (15 downto 0);
-           led : out  STD_LOGIC_VECTOR (15 downto 0);
-           dyp0 : out  STD_LOGIC_VECTOR (6 downto 0);
-           dyp1 : out  STD_LOGIC_VECTOR (6 downto 0);
            ram1oe : out  STD_LOGIC;
            ram1we : out  STD_LOGIC;
            ram1en : out  STD_LOGIC;
@@ -56,7 +53,10 @@ architecture Behavioral of TestIOSerial is
     type TopState is (
         TStateStart,
         TStateGetAddr,
-        TStateReadCom,
+        TStateReadCom0,
+        TStateReadCom1,
+        TStateReadCom2,
+        TStateReadCom3,
         TStateWriteRam0,
         TStateWriteRam1,
         TStateReadRam0,
@@ -68,64 +68,60 @@ architecture Behavioral of TestIOSerial is
         TStateWriteCom4,
         TStateDone
     );
-
-    type ComReadState is (
-        CStateRead0,
-        CStateRead1,
-        CStateRead2,
-        CStateRead3
-    );
     
     signal stateTop : TopState := TStateStart;
-    signal stateComRead : ComReadState := CStateRead0;
 
-    signal tempLed  : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
     signal tempAddr : STD_LOGIC_VECTOR(17 downto 0) := (others => '0');
     signal tempData : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
     signal readData : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
     signal temp1oe, temp1en, temp1we : STD_LOGIC := '1';
     signal tempWrn  : STD_LOGIC := '1';
     signal tempRdn  : STD_LOGIC := '1';
-    signal comReadStart : STD_LOGIC := '0';
-    signal comReadValid : STD_LOGIC := '0';
 
 begin
-    top: process (clk, rst)
+    top: process (comclk, rst)
     begin
         if (rst = '0') then
             temp1en <= '1';
             temp1oe <= '1';
             temp1we <= '1';
-            dyp0 <= (others => '0');
-            dyp1 <= (others => '0');
             stateTop <= TStateStart;
-            tempLed <= (others => '0');
-        elsif rising_edge(clk) then
+        elsif rising_edge(comclk) then
             case stateTop is
                 when TStateStart =>
                     stateTop <= TStateGetAddr;
                 when TStateGetAddr =>
                     tempAddr(15 downto 0) <= sw;
-                    tempLed <= sw;
-                    stateTop <= TStateReadCom;
+                    stateTop <= TStateReadCom0;
                     temp1en <= '1';
-                    comReadStart <= '1';
-                when TStateReadCom =>
-                    if comReadValid = '1' then
-                        tempLed(7 downto 0) <= readData(7 downto 0);
-                        tempData <= readData;
-                        comReadStart <= '0';
-                        stateTop <= TStateWriteRam0;
-                        temp1en <= '0';
+
+                when TStateReadCom0 =>
+                    stateTop <= TStateReadCom1;
+                when TStateReadCom1 =>
+                    tempRdn <= '1';
+                    ram1data <= (Others => 'Z');
+                    stateTop <= TStateReadCom2;
+                when TStateReadCom2 =>
+                    if data_ready = '1' then
+                        tempRdn <= '0';
+                        stateTop <= TStateReadCom3;
+                    else
+                        stateTop <= TStateReadCom1;
                     end if;
+                when TStateReadCom3 =>  
+                    readData(7 downto 0) <= ram1data(7 downto 0);
+                    tempRdn <= '1';
+                    tempData <= ram1Data;
+                    stateTop <= TStateWriteRam0;
+
                 when TStateWriteRam0 =>
+                    temp1en <= '0';
                     temp1we <= '1';
                     ram1data <= tempData;
                     ram1addr <= tempAddr;
                     stateTop <= TStateWriteRam1;
                 when TStateWriteRam1 =>
                     temp1we <= '0';
-                    tempLed <= tempAddr(7 downto 0) & tempData(7 downto 0);
                     stateTop <= TStateReadRam0;
                 when TStateReadRam0 =>
                     ram1data <= (others => 'Z');
@@ -135,7 +131,6 @@ begin
                     stateTop <= TStateReadRam1;
                 when TStateReadRam1 =>
                     tempData <= ram1data;
-                    tempLed <= tempAddr(7 downto 0) & ram1data(7 downto 0);
                     stateTop <= TStateWriteCom0;
                 when TStateWriteCom0 =>
                     ram1data <= readData + 1;
@@ -155,82 +150,12 @@ begin
                         stateTop <= TStateDone;
                     end if;
                 when TStateDone =>
-                    tempLed <= (others => '0');
                     stateTop <= TStateStart;
                 when others =>
                     stateTop <= TStateStart;
             end case;
         end if;
         
-        case stateTop is
-            when TStateStart => 
-                dyp0 <= "0111111";
-            when TStateGetAddr =>
-                dyp0 <= "0000110";
-            when TStateReadCom =>
-                dyp0 <= "1011011";
-            when TStateWriteRam0 =>
-                dyp0 <= "1001111";
-            when TStateWriteRam1 =>
-                dyp0 <= "1100110";
-            when TStateReadRam0 =>
-                dyp0 <= "1101101";
-            when TStateReadRam1 =>
-                dyp0 <= "1111101";
-            when TStateWriteCom0 =>
-                dyp0 <= "0000111";
-            when TStateWriteCom1 =>
-                dyp0 <= "0000111";
-            when TStateWriteCom2 =>
-                dyp0 <= "0000111";
-            when TStateWriteCom3 =>
-                dyp0 <= "0000111";
-            when TStateWriteCom4 =>
-                dyp0 <= "0000111";
-            when TStateDone =>
-                dyp0 <= "1111111";
-            when others =>
-                dyp0 <= "0111111";
-        end case;
-    end process;
-    
-    comread: process (comclk, rst)
-    begin
-        if rst = '0' then
-            comReadValid <= '0';
-            tempRdn <= '1';
-            readData <= (others => '0');
-            stateComRead <= CStateRead0;
-        elsif rising_edge(comclk) then
-            if stateTop = TStateReadCom and comReadStart = '1' then
-                case stateComRead is
-                    when CStateRead0 =>
-                        stateComRead <= CStateRead1;
-                    when CStateRead1 =>
-                        tempRdn <= '1';
-                        ram1data <= (Others => 'Z');
-                        stateComRead <= CStateRead2;
-                    when CStateRead2 =>
-                        if data_ready = '1' then
-                            tempRdn <= '0';
-                            stateComRead <= CStateRead3;
-                        else
-                            stateComRead <= CStateRead1;
-                        end if;
-                    when CStateRead3 =>
-                        readData(7 downto 0) <= ram1data(7 downto 0);
-                        tempRdn <= '1';
-                        comReadValid <= '1';
-                        stateComRead <= CStateRead0;
-                    when others =>
-                        comReadValid <= '0';
-                        stateComRead <= CStateRead0;
-                end case;
-            else
-                comReadValid <= '0';
-                stateComRead <= CStateRead0;
-            end if;
-        end if;
     end process;
 
     ram1en <= temp1en;
@@ -238,7 +163,6 @@ begin
     ram1we <= temp1we;
     wrn <= tempWrn;
     rdn <= tempRdn;
-    led <= tempLed;
 
 end Behavioral;
 
