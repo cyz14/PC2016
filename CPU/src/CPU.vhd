950f9b3e563e9530e75a12dc5c25e179eabdf7d7
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-22 13:13:58 +0800

    添加了VGA的必要文件

--- CPU/src/CPU.vhd
+++ CPU/src/CPU.vhd
@@ -1,9 +1,10 @@
 -- CPU.vhd
 
 ENTITY CPU IS PORT (
-    CLK :  IN  STD_LOGIC;
-    RST :  IN  STD_LOGIC;
-    INT :  IN  STD_LOGIC;
+    CLK     :    IN    STD_LOGIC;
+    CLK_50  :    IN    STD_LOGIC;
+    RST     :    IN    STD_LOGIC;
+    INT     :    IN    STD_LOGIC;
     Ram1_en:     OUT   STD_LOGIC;
     Ram1_oe:     OUT   STD_LOGIC;
     Ram1_we:     OUT   STD_LOGIC;
@@ -14,6 +15,11 @@ ENTITY CPU IS PORT (
     Ram2_we:     OUT   STD_LOGIC;
     RAM2_Addr:   OUT   STD_LOGIC_VECTOR(17 downto 0);
     Ram2_Data:   INOUT STD_LOGIC_VECTOR(15 downto 0);
+    VGA_R:       OUT   STD_LOGIC_VECTOR( 2 downto 0);
+    VGA_G:       OUT   STD_LOGIC_VECTOR( 2 downto 0);
+    VGA_B:       OUT   STD_LOGIC_VECTOR( 2 downto 0);
+    Hs:          OUT   STD_LOGIC;
+    Vs:          OUT   STD_LOGIC;
     DebugInfo:   OUT   STD_LOGIC_VECTOR(15 downto 0)
 );
 END CPU;
@@ -26,8 +32,7 @@ ARCHITECTURE Behaviour OF CPU IS
         B  :  IN  STD_LOGIC_VECTOR(15 downto 0);
         OP :  IN  STD_LOGIC_VECTOR(4  downto 0);
         F  :  OUT STD_LOGIC_VECTOR(15 downto 0);
-        Zero: OUT STD_LOGIC
-    );
+        Zero: OUT STD_LOGIC);
     END Component;
 
     Component RegisterFile IS PORT (
@@ -38,8 +43,7 @@ ARCHITECTURE Behaviour OF CPU IS
         WriteData:  IN  STD_LOGIC_VECTOR(15 downto 0);
         ReadDataA:  OUT STD_LOGIC_VECTOR(15 downto 0);
         ReadDataB:  OUT STD_LOGIC_VECTOR(15 downto 0);
-        WriteEn:    OUT STD_LOGIC_VECTOR(15 downto 0)
-    );
+        WriteEn:    OUT STD_LOGIC_VECTOR(15 downto 0));
     END Component;
 
     Component InstructionMemory IS PORT (
@@ -48,10 +52,50 @@ ARCHITECTURE Behaviour OF CPU IS
         WE       :  OUT    STD_LOGIC;
         OE       :  OUT    STD_LOGIC;
         ReadAddr :  IN     STD_LOGIC_VECTOR(17 downto 0);
-        ReadData :  INOUT  STD_LOGIC_VECTOR(15 downto 0)
-    );
+        ReadData :  INOUT  STD_LOGIC_VECTOR(15 downto 0));
     END Component;
 
+    Component video_sync IS PORT (
+        clock:                              IN STD_LOGIC;	-- should be 25M Hz
+        video_on, Horiz_Sync, Vert_Sync:    OUT STD_LOGIC;
+        H_count_out, V_count_out:			OUT STD_LOGIC_VECTOR(9 downto 0));
+    END Component;
+
+    SIGNAL clock_25 :                       STD_LOGIC;
+    SIGNAL video_on:                        STD_LOGIC;
+    SIGNAL t_hsync, t_vsync:                STD_LOGIC;
+    SIGNAL H_count, V_count:                STD_LOGIC_VECTOR(9 downto 0);
+    
+    CONSTANT zero3:         STD_LOGIC_VECTOR(2 downto 0) := "000";
+    CONSTANT background_r:  STD_LOGIC_VECTOR(2 downto 0) := zero3;
+    CONSTANT background_g:  STD_LOGIC_VECTOR(2 downto 0) := zero3;
+    CONSTANT background_b:  STD_LOGIC_VECTOR(2 downto 0) := zero3;
+
+
 BEGIN
+
+    PROCESS --  50M to 25M
+    BEGIN
+        WAIT UNTIL CLK_50'Event AND CLK_50 = '1';    
+        clock_25 <= NOT clock_25;
+    END PROCESS;
+
+    sync: Video_Sync PORT MAP (
+        clock => clock_25,
+        horiz_sync => t_hsync,
+        vert_sync => t_vsync,
+        video_on => video_on,
+        H_count_out => H_count,
+        V_count_out => V_count
+    );
+    Hs <= t_hsync;
+    Vs <= t_vsync;
+
+    DrawScreen: PROCESS(clock_25)
+    BEGIN
+        VGA_R <= background_r;
+        VGA_G <= background_g;
+        VGA_B <= background_b;
+    END PROCESS;
 
 END Behaviour;
---
+++ CPU/test/ALU.vhd
@@ -0,0 +1,110 @@
+-- ALU.vhd
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+ENTITY ALU IS
+PORT (
+    A  :  IN  STD_LOGIC_VECTOR(15 downto 0);
+    B  :  IN  STD_LOGIC_VECTOR(15 downto 0);
+    OP :  IN  STD_LOGIC_VECTOR(4  downto 0);
+    F  :  OUT STD_LOGIC_VECTOR(15 downto 0);
+    T  :  OUT STD_LOGIC
+);
+END ALU;
+
+ARCHITECTURE Behaviour OF ALU IS
+
+    Type ALUOP IS (
+        OP_NONE,-- No operation 
+        OP_ADD, -- F <= A  +  B
+        OP_SUB, -- F <= A  -  B
+        OP_AND, -- F <= A  &  B
+        OP_OR,  -- F <= A  |  B
+        OP_XOR, -- F <= A xor B
+        OP_CMP, -- F <= A !=  B, not equal
+        OP_LT,  -- F <= A  <  B
+        OP_POS, -- F <= A
+        OP_SLL, -- F <= A <<  B
+        OP_SRL, -- F <= A >>  B(logical)
+        OP_SRA, -- F <= A >>  B(arith)
+        );
+
+    CONSTANT ONE16  : STD_LOGIC_VECTOR(15 downto 0) := CONV_STD_LOGIC_VECTOR(1, 16);
+    CONSTANT ZERO16 : STD_LOGIC_VECTOR(15 downto 0) := CONV_STD_LOGIC_VECTOR(0, 16);
+
+    SIGNAL tempNUL: STD_LOGIC_VECTOR(16 downto 0)   := CONV_STD_LOGIC_VECTOR(0, 17);
+    SIGNAL tempADD: STD_LOGIC_VECTOR(16 downto 0)   := CONV_STD_LOGIC_VECTOR(0, 17);
+    SIGNAL tempSUB: STD_LOGIC_VECTOR(16 downto 0)   := CONV_STD_LOGIC_VECTOR(0, 17);
+    SIGNAL tempAND: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempOR : STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempXOR: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempCMP: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempLT : STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempPOS: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempSLL: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempSRL: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempSRA: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16;
+    SIGNAL tempROL: STD_LOGIC_VECTOR(15 downto 0)   := ZERO16; -- useless
+
+BEGIN
+    tempNUL <= ZERO16;
+    tempADD <= STD_LOGIC_VECTOR(ieee.numeric_std.unsigned('0' & A) + ieee.numeric_std.unsigned('0' & B));
+    tempSUB <= STD_LOGIC_VECTOR(ieee.numeric_std.unsigned('0' & A) - ieee.numeric_std.unsigned('0' & B));
+    tempAND <= A AND B;
+    tempOR  <= A OR  B;
+    tempXOR <= A XOR B;
+    tempCMP <= ZERO16 WHEN A = B ELSE ONE16;
+    tempLT  <= ONE16  WHEN A < B ELSE ZERO16;
+    tempPOS <= A;
+    tempSLL <= TO_STDLOGICVECTOR(TO_BITVECTOR(A) SLL CONV_INTEGER(B));
+    tempSRL <= TO_STDLOGICVECTOR(TO_BITVECTOR(A) SRL CONV_INTEGER(B));
+    tempSRA <= TO_STDLOGICVECTOR(TO_BITVECTOR(A) SRA CONV_INTEGER(B));
+    tempROL <= TO_STDLOGICVECTOR(TO_BITVECTOR(A) ROL CONV_INTEGER(B));
+    
+    PROCESS (A, B, OP)
+    BEGIN
+        CASE OP IS
+            WHEN OP_NONE => 
+                F <= ZERO16;
+                T <= '0'; -- 
+            WHEN OP_ADD  => 
+                F <= tempADD(15 downto 0);
+                T <= tempADD(16), -- 
+            WHEN OP_SUB  => 
+                F <= tempSUB(15 downto 0);
+                T <= temp, -- F <= A  -  B
+            WHEN OP_AND  => 
+                F <= tempAND;
+                T <= '0'; -- F <= A  &  B
+            WHEN OP_OR   => ;
+                F <= tempOR;
+                T <= '0'; -- F <= A  |  B
+            WHEN OP_XOR  => 
+                F <= tempXOR;
+                T <= '0'; -- F <= A xor B
+            WHEN OP_CMP  => 
+                F <= tempCMP;
+                T <= tempCMP(0); -- F <= A !=  B, not equal
+            WHEN OP_LT   => 
+                F <= tempLT;
+                T <= tempLT(0);  -- F <= A  <  B
+            WHEN OP_POS  => 
+                F <= tempPOS;
+                T <= '0';        -- F <= A
+            WHEN OP_SLL  => 
+                F <= tempSLL;
+                T <= '0';        -- F <= A <<  B
+            WHEN OP_SRL  => 
+                F <= tempSRL;
+                T <= '0';        -- F <= A >>  B(logical)
+            WHEN OP_SRA  => 
+                F <= tempSRA; 
+                T <= '0';        -- F <= A >>  B(arith)
+        END CASE;
+    END PROCESS;
+
+END Behaviour;
