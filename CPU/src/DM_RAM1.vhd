1e14a666956bd7fd9ffe86af739dc1ca84283979
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-25 18:15:28 +0800

    还没完成DM_Ram1

--- CPU/src/DM_RAM1.vhd
+++ CPU/src/DM_RAM1.vhd
@@ -19,6 +19,7 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -29,6 +30,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+use WORK.COMMON.ALL;
+
 entity DM_RAM1 is port (
     CLK:            IN   STD_LOGIC;
     RST:            IN   STD_LOGIC;
@@ -36,7 +39,7 @@ entity DM_RAM1 is port (
     MemWE:          IN   STD_LOGIC;
     MemWriteData:   IN   STD_LOGIC_VECTOR(15 downto 0);
     MemRead:        IN   STD_LOGIC;
-    ALUOut:         IN   STD_LOGIC_VECTOR(15 downto 0);
+    ALUOut:         IN   STD_LOGIC_VECTOR(15 downto 0); -- if MemWE, this is Address
     DstVal:         OUT  STD_LOGIC_VECTOR(15 downto 0);
     
     Ram1OE:         OUT   STD_LOGIC;
@@ -67,23 +70,62 @@ end DM_RAM1;
 
 architecture Behavioral of DM_RAM1 is
 
+    
     SIGNAL IsWriteMode : STD_LOGIC;
-    SIGNAL IsComMode   : STD_LOGIC;
+    SIGNAL IsUartWrite : STD_LOGIC;
     SIGNAL IsVGAWrite  : STD_LOGIC;
     
-
+    SIGNAL UartReadyLoad : STD_LOGIC;
+    SIGNAL UartUpRead    : STD_LOGIC;
+    
+    SIGNAL LastException : STD_LOGIC;
+    
 begin
     Ram1EN <= '0';
-    Ram1Addr <= (others => '0');
+    Ram1Addr(17 downto 16) <= (others => '0');
+    
+    Ram1WE <= not IsWriteMode or 
     
     Process(CLK, RST)
     BEGIN
         if RST = '0' THEN
             Ram1OE <= '0';
             Ram1Data <= (others => 'Z');
+            IsWriteMode <= '0';
+            IsUartWrite <= '0';
+            IsVGAWrite  <= '0';
+            UartReadyLoad <= '0';
+            UartUpRead <= '1';
             
-        ELSIF CLK'event and CLK = 0 THEN
+        ELSIF CLK'event and CLK = 1 THEN
+            Ram1OE <= not MemWE;
+            Ram1Addr(15 downto 0) <= ALUOut;
+            IsUartWrite <= '0';
+            IsVGAWrite  <= '0';
+            UartReadyLoad <= '0';
+            IsWriteMode <= MemWE;
+            LastException <= '0';
+
+            if MemWE = RAM_WRITE_ENABLE then
+                Ram1Data <= MemWriteData;
+                Ram1Addr(15 downto 0) <= ALUOut;
+            else
+                Ram1Data <= (others => 'Z');
+            end if;
             
+            if InstRead = '1' then
+                if DstAddr = x"BF00" then -- Write Uart
+                    
+                elsif DstAddr = x"BF01" then
+                
+                end if;
+            elsif MemRead = RAM_READ_ENABLE then
+                if DstAddr = x"BF00" then
+                    
+                elsif DstAddr = x"BF01" then
+                    
+                end if;
+            end if;
         END IF;
     END Process;
     
