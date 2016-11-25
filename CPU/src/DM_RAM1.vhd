8b4ba389d170187a72beeadb00d8f724d5772ee0
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-25 20:48:37 +0800

    初步完成了DM_Ram1，准备开始测试

--- CPU/src/DM_RAM1.vhd
+++ CPU/src/DM_RAM1.vhd
@@ -40,6 +40,10 @@ entity DM_RAM1 is port (
     MemWriteData:   IN   STD_LOGIC_VECTOR(15 downto 0);
     MemRead:        IN   STD_LOGIC;
     ALUOut:         IN   STD_LOGIC_VECTOR(15 downto 0); -- if MemWE, this is Address
+    
+    InstRead:       IN   STD_LOGIC;
+    InstVal:        IN   STD_LOGIC_VECTOR(15 downto 0);
+    
     DstVal:         OUT  STD_LOGIC_VECTOR(15 downto 0);
     
     Ram1OE:         OUT   STD_LOGIC;
@@ -70,7 +74,14 @@ end DM_RAM1;
 
 architecture Behavioral of DM_RAM1 is
 
-    
+    SIGNAL LastALUOut: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL ALUOutMask: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL MemOutMask: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL InstOutMask: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL UartOutMask: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL KeyboardOutMask: STD_LOGIC_VECTOR(15 downto 0);
+    SIGNAL UartOut: STD_LOGIC_VECTOR(15 downto 0);
+
     SIGNAL IsWriteMode : STD_LOGIC;
     SIGNAL IsUartWrite : STD_LOGIC;
     SIGNAL IsVGAWrite  : STD_LOGIC;
@@ -80,11 +91,37 @@ architecture Behavioral of DM_RAM1 is
     
     SIGNAL LastException : STD_LOGIC;
     
+    SIGNAL SyncClk : STD_LOGIC;
+    SIGNAL SyncClkWorking : STD_LOGIC;
+    SIGNAL SyncClkA : STD_LOGIC;
+    SIGNAL SyncClkB : STD_LOGIC;
+    
 begin
+
+    SyncClk <= SyncClkA xor SyncClkB;
+    
     Ram1EN <= '0';
     Ram1Addr(17 downto 16) <= (others => '0');
+    Ram1WE <= not IsWriteMode or SyncClk;
+    wrn <= not IsUartWrite or SyncClk;
+    vga_wrn <= not IsVGAWrite or SyncClk;
+    vga_data <= MemWriteData;
+    rdn <= UartUpRead or not UartReadyLoad;
+    DstVal <= (LastALUOut and ALUOutMask) 
+            or (Ram1Data and MemOutMask) 
+            or (InstVal and InstOutMask) 
+            or (UartOut and UartOutMask) 
+            or (keyboard_val and KeyboardOutMask);
     
-    Ram1WE <= not IsWriteMode or 
+    UartOut(15 downto 2) <= (others => '0');
+    with data_ready select UartOut(1) <=
+        '1' when '1',
+        '0' when others;
+    with tbre and tsre select UartOut(0) <= 
+        '1' when '1',
+        '0' when others;
+        
+    Exception <= LastException;
     
     Process(CLK, RST)
     BEGIN
@@ -96,16 +133,21 @@ begin
             IsVGAWrite  <= '0';
             UartReadyLoad <= '0';
             UartUpRead <= '1';
-            
-        ELSIF CLK'event and CLK = 1 THEN
+            LastException <= '0';
+            SyncClkWorking <= '0';
+            SyncClkA <= '1';
+            SyncClkB <= '0';
+        ELSIF CLK'event and CLK = '1' THEN
+            SyncClkWorking <= '1';
+            SyncClkA <= not SyncClkA;
+            LastALUOut <= ALUOut;
             Ram1OE <= not MemWE;
             Ram1Addr(15 downto 0) <= ALUOut;
             IsUartWrite <= '0';
             IsVGAWrite  <= '0';
             UartReadyLoad <= '0';
-            IsWriteMode <= MemWE;
+            IsWriteMode <= not MemWE;
             LastException <= '0';
-
             if MemWE = RAM_WRITE_ENABLE then
                 Ram1Data <= MemWriteData;
                 Ram1Addr(15 downto 0) <= ALUOut;
@@ -113,19 +155,58 @@ begin
                 Ram1Data <= (others => 'Z');
             end if;
             
-            if InstRead = '1' then
-                if DstAddr = x"BF00" then -- Write Uart
-                    
-                elsif DstAddr = x"BF01" then
-                
+            KeyboardOutMask <= (others => '0');
+            if InstRead = '1' then -- read InstructionMemory
+                InstOutMask <= (others => '1');
+                ALUOutMask <= (others => '0');
+                MemOutMask <= (others => '0');
+                UartOutMask <= (others => '0');
+            elsif MemRead = RAM_READ_ENABLE then -- Read DataMemory
+                InstOutMask <= (others => '0');
+                ALUOutMask <= (others => '0');
+                MemOutMask <= (others => '1');
+                UartOutMask <= (others => '0');
+                if ALUOut = x"BF00" then -- read from Uart
+                    Ram1OE <= '1';
+                    UartReadyLoad <= '1';
+                    Ram1Data(15 downto 8) <= (others => '0');
+                    if not (data_ready = '1') then
+                        LastException <= '1';
+                        ExceptPC <= NowPC;
+                        UartReadyLoad <= '0';
+                    end if;
+                elsif ALUOut = x"BF01" then -- Read Uart Flag
+                    MemOutMask <= (others => '0');
+                    UartOutMask <= (others => '1');
+                elsif ALUOut = x"BF04" then -- Read Keyboard Input Key
+                    MemOutMask <= (others => '0');
+                    KeyboardOutMask <= (others => '1');
                 end if;
-            elsif MemRead = RAM_READ_ENABLE then
-                if DstAddr = x"BF00" then
-                    
-                elsif DstAddr = x"BF01" then
-                    
+            else -- Write Mem or do nothing
+                InstOutMask <= (others => '0');
+                ALUOutMask <= (others => '1');
+                MemOutMask <= (others => '0');
+                UartOutMask <= (others => '0');
+                if MemWE = RAM_WRITE_ENABLE and ALUOut = x"BF00" then -- write uart
+                    IsWriteMode <= '0';
+                    IsUartWrite <= '1';
+                    if not((tbre and tsre) = '1') then
+                        LastException <= '1';
+                        ExceptPC <= NowPC;
+                        IsUartWrite <= '0';
+                    end if;
+                elsif MemWE = RAM_WRITE_ENABLE and ALUOut = x"BF06" then -- write VGA
+                    IsWriteMode <= '0';
+                    IsVGAWrite <= '1';
                 end if;
             end if;
+            
+            -- if LastException = '1' then
+                -- UartReadyLoad <= '0';
+                -- IsWriteMode <= '0';
+                -- IsUartWrite <= '0';
+                -- LastException <= '0';
+            -- end if;
         END IF;
     END Process;
     
