bd66abbe10f363e58506f7a550559db6a667d21c
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-30 17:58:16 +0800

    新写了一个十六进制行数和指令的代码翻译py

--- CPU/src/test/read_bin.py
+++ CPU/src/test/read_bin.py
@@ -1,18 +1,23 @@
 
 import sys
 
-if len(sys.argv) > 0:
-    print len(sys.argv)
+if len(sys.argv) < 2:
+    print "Need bin file name" # len(sys.argv)
+
 
 for _, argc in enumerate(sys.argv):
-    print argc
-    
-print
+    pass # print argc
 
 if sys.argv[1].endswith(".o"):
     file = open(sys.argv[1], "rb")
 else:
     file = open("test.o", "rb")
+
+line_num_in_hex = False
+out_fmt_str = '%s%2d %2d %2d %2d'
+if "-lx" in sys.argv:
+    line_num_in_hex = True
+    out_fmt_str = '%s%X %X %X %X'
 
 data = file.read(-1)
 insl = map(ord, data)
@@ -20,4 +25,7 @@ num = len(insl) / 2
 for i in xrange(num):
     p0 = insl[i + i + 1]
     p1 = insl[i + i]
-    print '%d %d %d %d' % (p0 / 16, p0 % 16, p1 / 16, p1 % 16)
+    line_num = ""
+    if line_num_in_hex:
+        line_num = "%04x: " % i
+    print out_fmt_str % (line_num, p0 / 16, p0 % 16, p1 / 16, p1 % 16)
